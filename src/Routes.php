2652fd97b79d8f1a5de03ad4c462c52e66b66bf7
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-07-20 16:37:25 +0800

    added routes in autoload files

--- src/Routes.php
+++ src/Routes.php
@@ -1,5 +1,5 @@
 <?php
 
-require __DIR__ . '/../config/routes/auth.php';
+require __DIR__ . '/../../../../config/routes/auth.php';
 require __DIR__ . '/database/migrations/routes/migration.php';
-require __DIR__ . '/../config/routes/web.php';
+require __DIR__ . '/../../../../config/routes/web.php';
