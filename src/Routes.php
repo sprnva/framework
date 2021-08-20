5ef16a19a691ce99d60eaca7c12eb2d39c449a6f
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-08-20 22:48:01 +0800

    Update Routes.php

--- src/Routes.php
+++ src/Routes.php
@@ -1,11 +1,7 @@
 <?php
 
-if (file_exists(__DIR__ . '/../../fortify/src/routes/auth.php')) {
-    require __DIR__ . '/../../fortify/src/routes/auth.php';
-} else {
-    if (file_exists(__DIR__ . '/../../../../config/routes/auth.php')) {
-        require __DIR__ . '/../../../../config/routes/auth.php';
-    }
+if (file_exists(__DIR__ . '/../../../../config/routes/auth.php')) {
+    require __DIR__ . '/../../../../config/routes/auth.php';
 }
 
 if (file_exists(__DIR__ . '/database/migrations/routes/migration.php')) {
