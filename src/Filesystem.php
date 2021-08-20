bcc9bdf8c0f064aa378e5a5e41fa07466ef850ff
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-08-20 22:45:37 +0800

    added copy fuction and recursive dir copy

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -54,6 +54,18 @@ class Filesystem
     }
 
     /**
+     * Copy a file to a new location.
+     *
+     * @param  string  $path
+     * @param  string  $target
+     * @return bool
+     */
+    public function copy($path, $target)
+    {
+        return copy($path, $target);
+    }
+
+    /**
      * Delete the file at a given path.
      *
      * @param  string|array  $paths
@@ -178,5 +190,61 @@ class Filesystem
     public function cleanDirectory($directory)
     {
         return $this->deleteDirectory($directory, true);
+    }
+
+    public function copyDirectory($directory, $destination, $options = '')
+    {
+        if (! $this->isDirectory($directory)) {
+            return false;
+        }
+
+        // If the destination directory does not actually exist, we will go ahead and
+        // create it recursively, which just gets the destination prepared to copy
+        // the files over. Once we make the directory we'll proceed the copying.
+        $this->ensureDirectoryExists($destination, 0777);
+
+        $items = new FilesystemIterator($directory, FilesystemIterator::SKIP_DOTS);
+
+        foreach ($items as $item) {
+
+            // As we spin through items, we will check to see if the current file is actually
+            // a directory or a file. When it is actually a directory we will need to call
+            // back into this function recursively to keep copying these nested folders.
+            $target = $destination.'/'.$item->getBasename().$options;
+
+            if ($item->isDir()) {
+                $path = $item->getPathname();
+
+                if (! $this->copyDirectory($path, $target, $options)) {
+                    return false;
+                }
+            }
+
+            // If the current items is just a regular file, we will just copy this to the new
+            // location and keep looping. If for some reason the copy fails we'll bail out
+            // and return false, so the developer is aware that the copy process failed.
+            else {
+                if (! $this->copy($item->getPathname(), $target)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Ensure a directory exists.
+     *
+     * @param  string  $path
+     * @param  int  $mode
+     * @param  bool  $recursive
+     * @return void
+     */
+    public function ensureDirectoryExists($path, $mode = 0755, $recursive = true)
+    {
+        if (! $this->isDirectory($path)) {
+            $this->makeDirectory($path, $mode, $recursive);
+        }
     }
 }
