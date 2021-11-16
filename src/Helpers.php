2dbd8bf8b2d3f95c5418d8eb7845563bd862e5c0
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2021-11-16 11:41:34 +0800

    Update Helpers.php

--- src/Helpers.php
+++ src/Helpers.php
@@ -353,12 +353,67 @@ if (!function_exists('abort')) {
      */
     function abort($code, $message = '')
     {
-        $data = [
-            'message' => ($message == "") ? error_page($code) : $message
-        ];
-        extract($data);
-        return require "vendor/sprnva/framework/src/Error.php";
+        $data = ($message == "") ? error_page($code) : $message;
+
+        echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>' . $code . ' | ' . $data . '</title><style>
+                body {
+                    height: 100%;
+                    background: #fff;
+                    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                    color: #4e7d50;
+                    font-weight: 300;
+                }
+        
+                h1 {
+                    font-weight: lighter;
+                    letter-spacing: 0.8;
+                    font-size: 2.3rem;
+                    margin-top: 0;
+                    margin-bottom: 0;
+                    color: #4e7d50;
+                }
+        
+                .wrap {
+                    margin-top: 20%;
+                    background: #fff;
+                    text-align: center;
+                }
+        
+                p {
+                    margin-top: 1.5rem;
+                }
+            </style>
+        </head>
+        <body><div class="wrap"><h1>' . $code . ' | ' . $data . '</h1></div></body></html>';
+
         die();
+    }
+}
+
+if (!function_exists('gate_denies')) {
+    /**
+     * abort and display error message
+     * 
+     */
+    function gate_denies($access = '', $message = '')
+    {
+        if ($_SESSION['AUTH']['role_id'] != null) {
+            if (!empty($_SESSION['AUTH']['roles'])) {
+                $authPermission = $_SESSION['AUTH']['roles']['permission'];
+
+                $permissionsList = [];
+                foreach (explode(',', $authPermission) as $permission) {
+                    $test = DB()->select("*", "permissions", "id = '$permission'")->get();
+                    $permissionsList[] =  $test['title'];
+                }
+
+                if (!in_array($access, $permissionsList)) {
+                    abort(403, $message);
+                }
+            } else {
+                abort(403, $message);
+            }
+        }
     }
 }
 
