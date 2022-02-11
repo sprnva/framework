37bd7dac766035e87827673b31701597e367909b
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-02-11 14:46:47 +0800

    [FIX] dd not showing in devtools preview tab

--- src/Dumper/Dumper.php
+++ src/Dumper/Dumper.php
@@ -7,7 +7,7 @@ class Dumper
     public static function dump()
     {
         $data = '';
-        $data .= '<style> .accordion {background-color: #eee;color: #444;cursor: pointer;padding: 18px;width: 100%;border: none;text-align: left;font-size: 15px;transition: 0.4s;} .drpDwn {cursor: pointer;color: #aaa;} .panel-data {padding: 0 18px;display: none;background-color: transparent;overflow: hidden;border: 0px;} .panel-data1 {padding: 0 18px;display: none;background-color: transparent;overflow: hidden;border: 0px;} .collections {font-size: 12px !important;font-family: monospace;font-weight: 300;background:#18171B;color: #FF8400; padding: 5px;word-break: break-word;margin-top: 10px;} .array_color {color: #4e9cde;} .data-color{color: #56DB3A;} </style>';
+        $data .= '<style>.accordion{background-color:#eee;color:#444;cursor:pointer;padding:18px;width:100%;border:none;text-align:left;font-size:15px;transition:0.4s}.drpDwn{cursor:pointer;color:#aaa}.panel-data{padding:0 18px;display:block;background-color:transparent;overflow:hidden;border:0}.panel-data1{padding:0 18px;display:block;background-color:transparent;overflow:hidden;border:0}.collections{font-size:12px!important;font-family:monospace;font-weight:300;background:#18171B;color:#FF8400;padding:5px;word-break:break-word;margin-top:10px}.array_color{color:#4e9cde}.data-color{color:#56DB3A}</style>';
 
         foreach (func_get_args() as $x) {
             if (is_array($x)) {
@@ -40,7 +40,7 @@ class Dumper
             }
         }
 
-        $data .= ' <script>var acc = document.getElementsByClassName("drpDwn");var i; for(i = 0; i < acc.length; i++){acc[i].innerHTML = "&#9658;";acc[i].addEventListener("click", function() {this.classList.toggle("active");var panel = this.nextElementSibling; if(panel.style.display === "block"){panel.style.display = "none";this.innerHTML = "&#9658;";}else{panel.style.display = "block";this.innerHTML = "&#9660;";}});}</script>';
+        $data .= '<script>var i,acc=document.getElementsByClassName("drpDwn");for(i=0;i<acc.length;i++){acc[i].innerHTML="&#9658;";var panel1=acc[i].nextElementSibling;acc[i].classList.toggle("active"),panel1.style.display="block",acc[i].innerHTML="&#9660;",acc[i].addEventListener("click",function(){var i=this.nextElementSibling;"block"===i.style.display?(i.style.display="none",this.innerHTML="&#9658;"):(i.style.display="block",this.innerHTML="&#9660;")})}</script>';
 
         return $data;
     }
