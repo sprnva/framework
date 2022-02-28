e0ddda7cd2aec8a8815068906b56686604bd2aa0
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-03-01 00:33:29 +0800

    Update Filesystem.php

--- src/Filesystem/Filesystem.php
+++ src/Filesystem/Filesystem.php
@@ -32,11 +32,11 @@ class Filesystem implements FilesystemInterface
     public static function makeDirectory($path, $mode = 0755, $recursive = false, $force = false)
     {
         if ($force) {
-            chmod(dirname(__DIR__) . "/" . $path, $mode);
+            chmod(dirname($path), $mode);
             return @mkdir($path, $mode, $recursive);
         }
 
-        chmod(dirname(__DIR__) . "/" . $path, $mode);
+        chmod(dirname($path), $mode);
         return mkdir($path, $mode, $recursive);
     }
 
@@ -219,6 +219,46 @@ class Filesystem implements FilesystemInterface
                 $path = $item->getPathname();
 
                 if (!$this->copyDirectory($path, $target, $options)) {
+                    return false;
+                }
+            }
+
+            // If the current items is just a regular file, we will just copy this to the new
+            // location and keep looping. If for some reason the copy fails we'll bail out
+            // and return false, so the developer is aware that the copy process failed.
+            else {
+                if (!$this->copy($item->getPathname(), $target)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function copyFortifyDirectory($directory, $destination, $options = '')
+    {
+        if (!$this->isDirectory($directory)) {
+            return false;
+        }
+
+        // If the destination directory does not actually exist, we will go ahead and
+        // create it recursively, which just gets the destination prepared to copy
+        // the files over. Once we make the directory we'll proceed the copying.
+
+        $items = new FilesystemIterator($directory, FilesystemIterator::SKIP_DOTS);
+
+        foreach ($items as $item) {
+
+            // As we spin through items, we will check to see if the current file is actually
+            // a directory or a file. When it is actually a directory we will need to call
+            // back into this function recursively to keep copying these nested folders.
+            $target = $destination . '/' . str_replace('.stubs', $options, $item->getBasename());
+
+            if ($item->isDir()) {
+                $path = $item->getPathname();
+
+                if (!$this->copyFortifyDirectory($path, $target, $options)) {
                     return false;
                 }
             }
